671698968896d5e8c329f89edd7dbe8c581d1753
Micael Rodrigues <micaelr95@outlook.pt>
2018-12-02 15:20:35 +0000

    Estrutura t_user

--- include/common.h
+++ include/common.h
@@ -3,6 +3,15 @@
 
 #define STRINGSIZE 20
 
+typedef struct
+{
+    int id;
+    char nome[STRINGSIZE];
+    char nif[STRINGSIZE];
+    char username[STRINGSIZE];
+    char masterpassword[STRINGSIZE];
+} t_user;
+
 void getusername(char *user);
 
 void getpassword(char *pass);
--- src/menulogin.c
+++ src/menulogin.c
@@ -5,18 +5,17 @@
 
 void menulogin(void)
 {
-    char user[STRINGSIZE];
-    char pass[STRINGSIZE];
+    t_user users[20];
     int opcao;
     do
     {
         printf("[1] Login\n");
         printf("[2] Criar Conta\n");
         printf("\n[0] Sair\n");
-        opcao = (int)((char)getch());
-        printf("A opção escolhida foi %d", opcao);
+        opcao = getch();
+        printf("A opção escolhida foi %c", opcao);
     }while(opcao!='1' && opcao!='2' && opcao!='0');
     //getusername(&user);
     //getpassword(&pass);
-    printf("\nOlá %s!\nA sua password é %s", user, pass);
+    printf("\n\n\n\n\Olá %s!\nA sua password é %s", users[0].username, users[0].masterpassword);
 }
