3ec608f26d1d22e25291afdae27225ba8510df9f
soficgomes <45452696+soficgomes@users.noreply.github.com>
2018-12-14 18:05:27 +0000

    verifica data e hora

--- src/common.c
+++ src/common.c
@@ -126,16 +126,46 @@ void clearscreen(void)
 t_date getDate()
 {
     t_date i_data;
-    printf("Data (dd/mm/aaaa): ");
-    scanf(" %2d/%2d/%4d", &i_data.dia, &i_data.mes, &i_data.ano);
+    printf("\nData (dd/mm/aaaa): ");
+    do{
+        fflush(stdin);
+        scanf(" %2d/%2d/%4d", &i_data.dia, &i_data.mes, &i_data.ano);
+        if (i_data.dia<01 || i_data.dia>31)
+        {
+            printf("Dia inválido! Insira de novo.\n");
+        }
+        else if (i_data.mes<01 || i_data.mes>12)
+        {
+            printf("Mês inválido! Insira de novo.\n");
+        }
+        else if (i_data.ano<1111)
+        {
+            printf("Ano inválido! Insira de novo.\n");
+        }
+    }while(i_data.dia<01 && i_data.dia>31 && i_data.mes<01 && i_data.mes>12 && i_data.ano<1111);
     return i_data;
 }
 
 t_hour getHour()
 {
     t_hour i_hora;
-    printf("Horas (hh:mm:ss): ");
-    scanf(" %2d:%2d:%2d", &i_hora.hora, &i_hora.minuto, &i_hora.segundo);
+    printf("\nHoras (hh:mm:ss): ");
+    do{
+        fflush(stdin);
+        scanf(" %2d:%2d:%2d", &i_hora.hora, &i_hora.minuto, &i_hora.segundo);
+        if (i_hora.hora<00 || i_hora.hora>23)
+        {
+            printf("Hora inválida! Insira de novo.\n");
+        }
+        else if (i_hora.minuto<00 || i_hora.minuto>60)
+        {
+            printf("Minuto inválido! Insira de novo.\n");
+        }
+        else if (i_hora.segundo<00 || i_hora.segundo>60)
+        {
+            printf("Segundo inválido! Insira de novo.\n");
+        }
+    }while(i_hora.hora<00 && i_hora.hora>23 && i_hora.minuto<00 && i_hora.minuto>60 && i_hora.segundo<00 && i_hora.segundo>60);
     return i_hora;
 }
 
