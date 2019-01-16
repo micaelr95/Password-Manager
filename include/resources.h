1063de67383340ecf3267ab10a90e2efb23416ef
soficgomes <45452696+soficgomes@users.noreply.github.com>
2019-01-16 21:37:39 +0000

    edit resource
    Co-Authored-By: Micael Rodrigues <micaelr95@users.noreply.github.com>

--- include/resources.h
+++ include/resources.h
@@ -9,4 +9,6 @@ void viewResource(t_resource arr_resource[], int num_res);
 
 void deleteResource(t_resource arr_resource[], int *num_ress);
 
+void editResource(t_resource arr_resource[], int num_ress);
+
 #endif // RESOURCES_H
